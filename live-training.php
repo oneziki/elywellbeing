6b2ecfa57a40e5d4ce8075dbcd830d041e789d4c
oneziki <oneziki@gmail.com>
2021-02-06 16:52:02 +0200

    More Pages Created

---
+++ live-training.php
@@ -0,0 +1,39 @@
+<!--Head Sections-->
+<?php include 'templates/head.php'; ?>
+<!--END Head Sections-->
+
+<!--Nav Sections-->
+<?php include 'templates/nav.php'; ?>
+<!--END Nav Sections-->
+
+<!--Header Sections-->
+<?php include "sections/header-section.php"; ?>
+<!--END Header Sections-->
+
+<section class="ftco-section ftco-intro" style="background-image: url(images/intro.jpg);">
+   <div class="container">
+      <div class="row justify-content-end">
+         <div class="col-md-6">
+            <div class="heading-section ftco-animate">
+               <h2 class="mb-4">Live Training</h2>
+               <h3 class="subheading">Ely Wellbeing</h3>
+            </div>
+            <p class="ftco-animate">
+            Live training is a trainer-led, face-to-face event in a room with clients who have travelled to a location to attend. Advance your circumstances or career by attending live events that offer you direct access to our industry experts. You'll gain insights in handling life skills and development, to support and help the people you live and work with positively, which serves all concerned.
+            </p>
+      </div>
+   </div>
+   <div class="container">
+      <div class="row justify-content-end">
+         <div class="col-md-6">
+            <div class="heading-section ftco-animate">
+               <a href="our-teachers.php" class="btn btn-primary">Learn More</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+<!--Footer Section-->
+<?php include 'templates/footer.php'; ?>
+<!--END Footer Section-->
